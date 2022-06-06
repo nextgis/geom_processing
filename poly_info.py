02da783280786516503d8be2cba7eb3f13067484
viktorjaa <viktorjaab21@gmail.com>
2022-06-06 18:48:40 +0300

    implement raising ValueError for string arguments, which are not wkt

--- poly_info.py
+++ poly_info.py
@@ -1,16 +1,19 @@
 from shapely import wkt
 from shapely.geometry import Polygon
+from shapely.errors import WKTReadingError
 
 
 def poly_info(obj_wkt):
-    obj_geom = wkt.loads(obj_wkt)
-    if not(obj_geom.geom_type == 'Polygon' or obj_geom.geom_type == 'MultiPolygon'):
-        raise ValueError("geom_type is not Polygon or MultiPolygon")
-    obj_info = {"area" : obj_geom.area,
-                "perimetr" : obj_geom.length,
-                 "BoundBox" : wkt.dumps(bb_polygon(obj_geom.bounds))}
-    return obj_info
-        
+    try:
+        obj_geom = wkt.loads(obj_wkt)
+        if not(obj_geom.geom_type == 'Polygon' or obj_geom.geom_type == 'MultiPolygon'):
+            raise ValueError("geom_type is not Polygon or MultiPolygon")
+        obj_info = {"area" : obj_geom.area,
+                    "perimetr" : obj_geom.length,
+                    "BoundBox" : wkt.dumps(bb_polygon(obj_geom.bounds))}
+        return obj_info
+    except WKTReadingError as e:
+        raise ValueError(str(e))
 
 def bb_polygon(bounds):
     v = [(bounds[x], bounds[y]) for (x, y) in [(0, 1), (0, 3), (2, 3), (2, 1)]]
