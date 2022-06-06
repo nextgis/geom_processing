d75e0edbcce559c249cc1d3a8dcbe92be22230c0
viktorjaa <viktorjaab21@gmail.com>
2022-06-06 16:56:24 +0300

    add tests for area, perimetr and other geometry type

--- test_poly_info.py
+++ test_poly_info.py
@@ -3,13 +3,34 @@ from poly_info import poly_info
 
 class TestPolyInfo(unittest.TestCase):
     def setUp(self):
-        self.polygon1 = 'Polygon((0 0, 1 0, 1 1, 0 1, 0 0))'
+        self.simple_polygon = 'Polygon((0 0, 1 0, 1 1, 0 1, 0 0))'
+        self.holey_polygon = 'Polygon((0 0, 4 0, 4 4, 0 4, 0 0), (1 1, 3 1, 3 3, 1 3, 1 1))'
+        self.multipolygon = 'MultiPolygon(((0 0, 1 0, 1 1, 0 1, 0 0)),((3 0, 7 3, 4 7, 0 4, 3 0)))'
+        self.linestring = 'LineString(0 0, 1 2, 3 4)'
 
-    def test_polygon1_area(self):
-        self.assertEquals(poly_info(self.polygon1)["area"], 1)
+    def test_simple_polygon_area(self):
+        self.assertEqual(poly_info(self.simple_polygon)["area"], 1)
         
-    def test_polygon1_perimetr(self):
-        self.assertEquals(poly_info(self.polygon1)["perimetr"], 4)
+    def test_simple_polygon_perimetr(self):
+        self.assertEqual(poly_info(self.simple_polygon)["perimetr"], 4)
+        
+    def test_holey_polygon_area(self):
+        self.assertEqual(poly_info(self.holey_polygon)["area"], 12)
+        
+    def test_holey_polygon_perimetr(self):
+        self.assertEqual(poly_info(self.holey_polygon)["perimetr"], 24)
+        
+    def test_multipolygon_area(self):
+        self.assertEqual(poly_info(self.multipolygon)["area"], 26)
+        
+    def test_multipolygon_perimetr(self):
+        self.assertEqual(poly_info(self.multipolygon)["perimetr"], 24)    
+        
+    def test_lisestring(self):
+        with self.assertRaises(ValueError) as context:
+            poly_info(self.linestring)
+            self.assertEquals(context.exception, ValueError)
+           
 
 if __name__ == '__main__':
     unittest.main()
