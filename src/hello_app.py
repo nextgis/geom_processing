bfd7cbbdda4e64454ab820bd9abb3b458549d926
viktorjaa <viktorjaab21@gmail.com>
2022-06-08 12:25:18 +0300

    implement post request for poly_info

--- src/hello_app.py
+++ src/hello_app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, request
+import json
+from poly_info import poly_info
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+    
+    
+@app.route("/handler", methods=['POST'])
+def handle_geom():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        try:
+            wkt1 = request.json["wkt1"]
+            return json.dumps(poly_info(wkt1))
+        except Exception as e:
+            return str(e)
+    else:
+        return 'Content-Type not supported!'
