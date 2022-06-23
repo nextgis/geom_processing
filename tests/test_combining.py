1a4cafaae1a7411e2f741d31504f9edf45b0ef4a
viktorjaa <viktorjaab21@gmail.com>
2022-06-23 16:18:07 +0300

    implement tests for many polygons

--- tests/test_combining.py
+++ tests/test_combining.py
@@ -25,6 +25,15 @@ class TestPolyInfo(unittest.TestCase):
         self.for_touch_1 = wkt.loads("MultiPolygon(((4 2, 4 3, 6 3, 6 2, 4 2)))")
         self.for_touch_2 = wkt.loads("MultiPolygon(((4 2, 6 3, 6 2, 4 2)))")
 
+        self.mp_3_b1 = wkt.loads("MultiPolygon(((0 0, 3 0, 3 1, 0 1, 0 0)))")
+        self.mp_3_b2 = wkt.loads("MultiPolygon(((0 1, 1 1, 1 3, 0 3, 0 1)))")
+        self.mp_3_n3 = wkt.loads("MultiPolygon(((2 1, 2 3, 3 3, 3 1, 2 1)))")
+        self.mp_3_z3 = wkt.loads("MultiPolygon(((2 1, 2 2, 1 2, 1 3, 3 3, 3 1, 2 1)))")
+
+        self.sq = wkt.loads("Multipolygon(((1 1, 1 3, 3 3, 3 1, 1 1)))")
+        self.up = wkt.loads("MultiPolygon(((2 2, 2 4, 4 4, 4 2, 3 3, 2 2)))")
+        self.down = wkt.loads("MultiPolygon(((2 2, 3 1, 4 2, 4 0, 2 0, 2 2)))")
+
     def test_simple_combining(self):
         res = combine(geoms=[self.simple_triangle_1, self.simple_triangle_2])
         correct = wkt.loads('MULTIPOLYGON (((1 0, 0 0, 0 1, 1 1, 1 0)))')
@@ -146,6 +155,44 @@ class TestPolyInfo(unittest.TestCase):
         correct = wkt.loads(correct_wkt)
         self.assertTrue(res.equals(correct))
 
+    def test_three_n(self):
+        res = combine([self.mp_3_b1, self.mp_3_b2, self.mp_3_n3])
+        correct_wkt = "MultiPolygon(((0 0, 0 3, 1 3, 1 1, 2 1, 2 3, 3 3, 3 0, 0 0)))"
+        correct = wkt.loads(correct_wkt)
+        self.assertTrue(res.equals(correct))
+
+    def test_three_z0(self):
+        res = combine([self.mp_3_b1, self.mp_3_b2, self.mp_3_z3], hole_area=0)
+        correct_wkt = "MultiPolygon(((0 0, 0 3, 3 3, 3 0, 0 0), (1 1, 2 1, 2 2, 1 2, 1 1)))"
+        correct = wkt.loads(correct_wkt)
+        self.assertTrue(res.equals(correct))
+
+    def test_three_z2(self):
+        res = combine([self.mp_3_b1, self.mp_3_b2, self.mp_3_z3], hole_area=2)
+        correct_wkt = "MultiPolygon(((0 0, 0 3, 3 3, 3 0, 0 0)))"
+        correct = wkt.loads(correct_wkt)
+        self.assertTrue(res.equals(correct))
+
+    def test_three_ud0(self):
+        res = combine([self.sq, self.up, self.down], hole_area=0)
+        correct_wkt = "MultiPolygon(((4 0, 2 0, 2 1, 1 1, 1 3, 2 3, 2 4, 4 4, 4 2, 4 0)," \
+                      "(4 2, 3 3, 3 1, 4 2)))"
+        correct = wkt.loads(correct_wkt)
+        print(res)
+        self.assertTrue(res.equals(correct))
+
+    def test_three_ud2(self):
+        res = combine([self.sq, self.up, self.down], hole_area=2)
+        correct_wkt = "MultiPolygon(((4 0, 2 0, 2 1, 1 1, 1 3, 2 3, 2 4, 4 4, 4 2, 4 0)))"
+        correct = wkt.loads(correct_wkt)
+        self.assertTrue(res.equals(correct))
+
+    def test_three_udh(self):
+        res = combine([self.with_hole, self.sq, self.up, self.down], hole_area=0)
+        correct_wkt = "MultiPolygon(((0 0, 0 5, 5 5, 5 0, 4 0, 2 0, 0 0), (1 3, 2 3, 2 4, 1 4, 1 3)," \
+                      "(3 3, 4 2, 3 1, 3 3)))"
+        correct = wkt.loads(correct_wkt)
+        self.assertTrue(res.equals(correct))
 
 if __name__ == '__main__':
     unittest.main()
