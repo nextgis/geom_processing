fa01e277afb43a011fc653337840a7fd04549d1a
viktorjaa <viktorjaab21@gmail.com>
2022-07-12 15:42:03 +0300

    implement two simple tests

--- src/simplification.py
+++ src/simplification.py
@@ -4,7 +4,7 @@ from shapely.geometry import Point, LineString, Polygon, MultiPolygon
 class StraightLine:
     def __init__(self, p1, p2):
         self.a = p1.y - p2.y
-        self.b = p2.x - p1.y
+        self.b = p2.x - p1.x
         self.c = p2.x * self.a + p2.y * self.b
 
     def inter_point(self, other):
@@ -21,46 +21,51 @@ class StraightLine:
 
 class ChangeList(list):
     def fill(self, geom):
-        for point in geom:
+        for point in geom[:-1]:
             change = self.ChangePoint(point)
             self.append(change)
         for elem in self:
-            self._calc_elem(elem)  # метод списка
+            self._calc_elem(elem)
         return self
 
     def _calc_elem(self, elem):
         index = self.index(elem)
-        next_ch = self[index + 1]
+        next_ch = self[(index + 1) % len(self)]
         prev_ch = self[index - 1]
         preprev_ch = self[index - 2]
         poly = self.polygonize()
-        elem.calc_changes(next_ch, prev_ch, preprev_ch, poly)
+        elem.calc_change(next_ch, prev_ch, preprev_ch, poly)
 
     def recalc_elem(self, elem):
         index = self.index(elem)
-        next_ch = self[index + 1]
+        next_ch = self[(index + 1) % len(self)]
         prev_ch = self[index - 1]
         preprev_ch = self[index - 2]
-        elem.update(next_ch, prev_ch, preprev_ch)
-        self._calc_elem(elem)
+        prev_ch.update(next_ch, elem, preprev_ch)
+        self.remove(elem)
         self._calc_elem(preprev_ch)
+        self._calc_elem(prev_ch)
+        self._calc_elem(next_ch)
+
+    def get_min(self):
+        return min([item for item in self if item.method in ("convex", "concave")],
+                   key=lambda x: x.area)
 
     def recalc_min(self):
-        mn = min(self, key=lambda x: x.area)
-        self.recalc_elem(mn)
+        self.recalc_elem(self.get_min())
 
     def polygonize(self):
-        return Polygon([item.point() for item in self])
+        return Polygon([item.point for item in self])
 
     class ChangePoint:
         def __init__(self, point):
-            self._point = point
+            self._point = Point(point)
             self._method = "no_method"
             self._area = float("inf")
 
         @property
         def point(self):
-            return self.point
+            return self._point
 
         @point.setter
         def point(self, point):
@@ -69,6 +74,10 @@ class ChangeList(list):
         @property
         def area(self):
             return self._area
+
+        @property
+        def method(self):
+            return self._method
 
         def calc_change(self, next_ch, prev_ch, preprev_ch, poly):
             try:
@@ -80,23 +89,18 @@ class ChangeList(list):
                     preprev_p = preprev_ch.point
                     convex_point = self._find_convex_point(preprev_p, prev_p,
                                                            self._point, next_p)
-                    self._area = Polygon([convex_point, next_p, preprev_p]).area
+                    self._area = Polygon([convex_point, self._point, prev_p]).area
                 else:
                     self._method = "concave"
                     self._area = Polygon([self.point, next_p, prev_p]).area
             except ValueError:
                 self._method = "no_method"
                 self._area = float("inf")
-                pass
-            except AttributeError:
-                self._method = "no_method"
-                self._area = float("inf")
-                raise AttributeError("Couldn't find neighbours point.")
 
         @staticmethod
         def _find_convex_point(preprev_p, prev_p, this_p, next_p):
-            l_prev = StraightLine(preprev_p.coords, prev_p.coords)
-            l_next = StraightLine(this_p.coords, next_p.coords)
+            l_prev = StraightLine(preprev_p, prev_p)
+            l_next = StraightLine(this_p, next_p)
             new_p = l_next.inter_point(l_prev)
             direct = ((next_p.x - prev_p.x) * (this_p.y - prev_p.y)
                       - (next_p.y - prev_p.y) * (this_p.x - prev_p.x))
@@ -110,19 +114,17 @@ class ChangeList(list):
             else:
                 raise ValueError("No point of intersection, edges are ||.")
 
-        def update(self, next_ch, prev_ch, preprev_ch):
+        def update(self, next_ch, elem_ch, prev_ch):
             try:
                 if self._method == "concave":
-                    del self
+                    self._point = elem_ch.point
                 elif self._method == "convex":
-                    preprev_p = preprev_ch.point
                     prev_p = prev_ch.point
+                    elem_p = elem_ch.point
                     next_p = next_ch.point
-                    convex_point = self._find_convex_point(preprev_p, prev_p,
-                                                           self.point, next_p)
+                    convex_point = self._find_convex_point(prev_p, self.point,
+                                                           elem_p, next_p)
                     self._point = convex_point
-                    del prev_ch
-
             except Exception as e:
                 print(e)
 
@@ -136,16 +138,17 @@ def get_changes(geoms):
 
 
 def get_change_of_min(changes):
-    mns = [min(change, key=lambda x: x.get_area()) for change in changes]
-    mn = min(mns, key=lambda x: x.get_area())
+    mns = [change.get_min() for change in changes]
+    mn = min(mns)
     index = mns.index(mn)
-    return changes[index]
+    return index
 
 
 def simplify(polygons, m):
     geoms = [list(polygon.exterior.coords) for polygon in polygons]
+    n = sum([len(geom) - 1 for geom in geoms])
     changes = get_changes(geoms)
-    for cnt in range(len(geoms) - m):
-        ch_m = get_change_of_min(changes)
-        ch_m.recalc_min()
+    for cnt in range(n - m):
+        index = get_change_of_min(changes)
+        changes[index].recalc_min()
     return MultiPolygon([change.polygonize() for change in changes])
--- tests/test_simplification.py
+++ tests/test_simplification.py
@@ -4,7 +4,17 @@ from src.simplification import *
 
 
 class TestSimplification(unittest.TestCase):
-    pass
+    def test_simple_convex_polygon(self):
+        poly_6 = wkt.loads("Polygon((1 0, 2 0, 3 1, 2 2, 1 2, 0 1, 1 0))")
+        poly_5 = simplify([poly_6], 5)
+        correct = wkt.loads("MULTIPOLYGON (((1.5 -0.5, 3 1, 2 2, 1 2, 0 1, 1.5 -0.5)))")
+        self.assertTrue(poly_5.equals(correct))
+
+    def test_simple_concave_polygon(self):
+        poly_7 = wkt.loads("Polygon((1 0, 2 0, 3 1, 2 2, 1.5 1, 1 2, 0 1, 1 0))")
+        poly_6 = simplify([poly_7], 6)
+        correct = wkt.loads("MULTIPOLYGON (((1.5 -0.5, 3 1, 2 2, 1.5 1, 1 2, 0 1, 1.5 -0.5)))")
+        self.assertTrue(poly_6.equals(correct))
 
 
 if __name__ == '__main__':
